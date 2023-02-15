a9953b13120c05afee86995377b4879a1ad2fc88
Risav03 <needleweed03@gmail.com>
2023-02-15 18:45:59 +0530

    go go power rangers

--- beast-verse/pages/index.tsx
+++ beast-verse/pages/index.tsx
@@ -3,10 +3,18 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import Landing from "../components/landing"
+import { useEffect } from "react";
+
+// import 'aos-animations/dist/animations.min.css';
+// import 'aos-animations/dist/animations.min.js';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init();
+  }, [])
+
   return (
     <>
       <Head>
@@ -20,6 +28,7 @@ export default function Home() {
 <link rel="preconnect" href="https://fonts.googleapis.com"/>
 <link rel="preconnect" href="https://fonts.gstatic.com" />
 <link href="https://fonts.googleapis.com/css2?family=Ruslan+Display&display=swap" rel="stylesheet"></link>
+<link rel="stylesheet" href="bower_components/aos/dist/aos.css" />
 
       </Head>
       {/* <h2 className='text-cyan-400 bg-slate-700 text-center text-9xl font-bold'>Hello </h2> */}
