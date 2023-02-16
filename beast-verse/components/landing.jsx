7b6bcd5f2aa8b9dfa6fe79dda42aeb3be6b90903
Risav03 <needleweed03@gmail.com>
2023-02-16 10:26:33 +0530

    landing first edition

--- beast-verse/components/landing.jsx
+++ beast-verse/components/landing.jsx
@@ -10,10 +10,10 @@ function Landing(){
             <div className=" mx-auto relative top-[35vh] w-[45%] backdrop-blur-lg border-2 rounded-lg backdrop-brightness-[.8] pb-10">
                 <h1 className="text-center py-10 text-[5vw] text-white font-Ruslan">Beast Verse</h1>
                 <div className="grid grid-flow-col grid-cols-2 w-[80%] mx-auto">
-                    <Link href="https://opensea.io/collection/beast-verse-coupons" className=" w-[60%] mx-auto">
-                        <button className="bg-blue-300 font-Ruslan text-white hover:bg-blue-400 hover:text-[1.2vw] w-full rounded-lg border-2 pt-[1.4vw] pb-[1.3vw]">Get an Egg</button>
+                    <Link href="https://opensea.io/collection/beast-verse-coupons" className=" w-[70%] mx-auto">
+                        <button className="bg-blue-500 font-Ruslan text-white hover:bg-cyan-400 hover:text-[1.2vw] transition-all duration-100 w-full rounded-lg border-2 pt-[1.7vw] pb-[1.6vw]">Get an Egg</button>
                     </Link>
-                    <button className="bg-blue-300 font-Ruslan text-white hover:bg-blue-400 hover:text-[1.2vw] w-[60%] mx-auto rounded-lg border-2 pt-[1.4vw] pb-[1.3vw]" >Play Now!</button>
+                    <button className="bg-blue-500 font-Ruslan text-white hover:bg-cyan-400 hover:text-[1.2vw] transition-all duration-100 w-[70%] mx-auto rounded-lg border-2 pt-[1.7vw] pb-[1.6vw]" >Play Now!</button>
                 </div>
             </div>
         </div>
