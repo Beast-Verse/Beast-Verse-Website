6484ca92a571ba840c8d6e830d79e906df0b3660
Risav03 <needleweed03@gmail.com>
2023-02-17 15:15:02 +0530

    visuals

--- beast-verse/components/swamp.jsx
+++ beast-verse/components/swamp.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 
 function Swamp(){
-    return <div className="bg-gray-900">
+    return <div className="bg-gray-900 h-[50vw]">
 
-    <div className="w-[40%] ml-[10vw] pt-[10vw]">
+    <div>
+
+    <div className="w-[40%] relative left-[10vw] top-[6vw]">
+        <div className="z-10 relative">
     <h1 className="text-[3.5vw] font-Monstserrat font-medium text-white">Soggy <span className="text-emerald-400">Swamp</span></h1>
     <p className="leading-loose mt-4 text-[1.1vw] font-Montserrat text-white">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+    </div>
+    <div className="w-full h-[30vw] bg-slate-800 relative bottom-[30vw] rounded-2xl z-0 blur-3xl"></div>
     </div>  
+
+
+
+
+    </div>
+
 
     </div>
 }
