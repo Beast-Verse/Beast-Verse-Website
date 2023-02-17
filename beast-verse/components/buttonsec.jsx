b0633f5fae19a3782a03497dac9c778f1c05be86
Risav03 <needleweed03@gmail.com>
2023-02-17 14:49:36 +0530

    added another domain

--- beast-verse/components/buttonsec.jsx
+++ beast-verse/components/buttonsec.jsx
@@ -12,19 +12,20 @@ function Buttonsec(){
     const[show2, setShow2] = useState(false);
     const[show3, setShow3] = useState(false);
     const[show4, setShow4] = useState(false);
+    const[show5, setShow5] = useState(false);
 
 
     return <div>
         <div className="grid grid-flow-row grid-rows-2 bg-slate-800">
             
             <div className="grid grid-flow-col grid-cols-2">
-                <button onClick={() => setShow1(true) & setShow2(false) & setShow3(false) & setShow4(false)} className=" w-full mx-auto bg-green-400 font-Monstserrat  text-white">
+                <button onClick={() => setShow1(true) & setShow2(false) & setShow3(false) & setShow4(false) & setShow5(false)} className=" w-full mx-auto bg-green-400 font-Monstserrat  text-white">
                     <div className="bg-slate-800 transition-all hover:opacity-90 duration-500">
                         <h3 className="p-8 hover:text-green-400 transition-colors duration-500 font-medium text-[1.5vw] font-Monstserrat text-white ">Forest</h3>
                     </div>
                 </button>
                 
-                <button onClick={() => setShow2(true) & setShow1(false)& setShow3(false)&setShow4(false)} className=" w-full mx-auto bg-red-400 font-Monstserrat  text-white">
+                <button onClick={() => setShow2(true) & setShow1(false)& setShow3(false)&setShow4(false) & setShow5(false)} className=" w-full mx-auto bg-red-400 font-Monstserrat  text-white">
                 <div className="bg-slate-800 transition-all hover:opacity-90 duration-500">
                         <h3 className="p-8 hover:text-red-400 transition-colors duration-500 font-medium text-[1.5vw] font-Monstserrat text-white ">Lava</h3>
                     </div>
@@ -32,28 +33,38 @@ function Buttonsec(){
             </div>
 
             <div className="grid grid-flow-col grid-cols-2">
-                <button onClick={() => setShow1(false) & setShow2(false) & setShow3(true) & setShow4(false)} className=" w-full mx-auto bg-yellow-400 font-Monstserrat  text-white">
+                <button onClick={() => setShow1(false) & setShow2(false) & setShow3(true) & setShow4(false) & setShow5(false)} className=" w-full mx-auto bg-yellow-400 font-Monstserrat  text-white">
                 <div className="bg-slate-800 transition-all hover:opacity-90 duration-500">
                         <h3 className="p-8 hover:text-yellow-400 transition-colors duration-500 font-medium text-[1.5vw] font-Monstserrat text-white ">Desert</h3>
                     </div>
                 </button>
-                <button onClick={() => setShow1(false) & setShow2(false) & setShow3(false) & setShow4(true)} className=" w-full mx-auto bg-blue-400 font-Monstserrat  text-white">
+                <button onClick={() => setShow1(false) & setShow2(false) & setShow3(false) & setShow4(true) & setShow5(false)} className=" w-full mx-auto bg-emerald-400 font-Monstserrat  text-white">
                 <div className="bg-slate-800 transition-all hover:opacity-90 duration-500">
-                        <h3 className="p-8 hover:text-blue-400 transition-colors duration-500 font-medium text-[1.5vw] font-Monstserrat text-white ">Swamp</h3>
+                        <h3 className="p-8 hover:text-emerald-400 transition-colors duration-500 font-medium text-[1.5vw] font-Monstserrat text-white ">Swamp</h3>
+                    </div>
+                </button>
+            </div>
+
+            <div className="grid grid-flow-col grid-cols-2">
+                <button onClick={() => setShow1(false) & setShow2(false) & setShow3(false) & setShow4(false) & setShow5(true)} className=" w-full mx-auto bg-blue-200 font-Monstserrat  text-white">
+                <div className="bg-slate-800 transition-all hover:opacity-90 duration-500">
+                        <h3 className="p-8 hover:text-blue-200 transition-colors duration-500 font-medium text-[1.5vw] font-Monstserrat text-white ">Ice</h3>
+                    </div>
+                </button>
+                <button className=" w-full mx-auto bg-gray-400 font-Monstserrat  text-white">
+                <div className="bg-gray-500 transition-all  duration-500">
+                        <h3 className="p-8 animate-pulse transition-colors duration-500 font-medium text-[1.5vw] font-Monstserrat text-gray-400 ">Coming Soon</h3>
                     </div>
                 </button>
             </div>
 
         </div>
 
-        {show1 && 
-                <Forest/>}
-        {show2 && 
-                <Lava/>}
-                {show3 && 
-                <Desert/>}
-                {show4 && 
-                <Swamp/>}
+        {show1 && <Forest/>}
+        {show2 && <Lava/>}
+        {show3 && <Desert/>}
+        {show4 && <Swamp/>}
+        {show5 && <Ice/>}
     </div>
 }
 
--- beast-verse/components/ice.jsx
+++ beast-verse/components/ice.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 
 function Ice(){
-    return <div className="bg-blue-400">
-        <h1>Ice</h1>
-        
+    return <div className="bg-gray-900">
+
+    <div className="grid- grid-flow-col grid-cols-2">
+
+    <div className="w-[40%] ml-[10vw] pt-[10vw]">
+    <h1 className="text-[3.5vw] font-Monstserrat font-medium text-white">Chilly <span className="text-blue-200">Cliffs</span></h1>
+    <p className="leading-loose mt-4 text-[1.1vw] font-Montserrat text-white">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+    </div>  
+
+    <div className="bg-slate-400 w-[2vw] h-[2vw]">
+
+    </div>
+
+    </div>
+
+
     </div>
 }
 
