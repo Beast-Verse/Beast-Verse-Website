dd10edf2ad29d8fe8046054e7970ebb7e5135574
SayakSarkar02 <sayaksarkar73@gmail.com>
2023-03-03 23:10:50 +0530

    nav bar animate test

--- beast-verse/components/Navbar.jsx
+++ beast-verse/components/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {motion} from 'framer-motion'
 
 export default function Navbar() {
   return (
-    <div className='bg-white w-[70%] container mx-auto rounded-xl drop-shadow-xl'>
+    <motion.div
+    
+    initial={{y: -100}}
+    animate={{y: 0}}
+
+    className='bg-white w-[70%] container mx-auto rounded-xl drop-shadow-xl'>
         <div className='p-4 px-5 flex flex-row justify-between items-center'>
             <div className='font-black font-Montserrat text-xl text-blue-900 leading-none cursor-pointer'>
                 Logo Here
@@ -15,6 +21,6 @@ export default function Navbar() {
                 <li className='bg-gradient-to-br from-blue-600 to-blue-400 hover:to-blue-600 hover:from-blue-500 text-white p-2 rounded-lg cursor-pointer'>Pre-Register</li>
             </ul>
         </div>
-    </div>
+    </motion.div>
   )
 }
