cd1873d984b90b86aaaba48e668a76224cf8cde3
Risav03 <needleweed03@gmail.com>
2023-03-14 15:44:30 +0530

    Stats

--- beast-verse/src/components/Monsters.jsx
+++ beast-verse/src/components/Monsters.jsx
@@ -1,7 +1,7 @@
 function Monsters(){
     return(
         <div className="text-center">
-            <h2 className="font-Montserrat text-[7vw] text-white">Beasts</h2>
+            <h2 className="font-Montserrat f text-[7vw] text-white">Beasts</h2>
             <h3 className="animate-pulse text-slate-700 font-Montserrat text-[3vw]">Coming soon...</h3>
         </div>
     );
