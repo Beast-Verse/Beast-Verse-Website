7752785db2bab89d0eb0f8c00226163b0f784c38
Risav03 <needleweed03@gmail.com>
2023-03-14 15:44:45 +0530

    stats

--- beast-verse/src/components/stats.jsx
+++ beast-verse/src/components/stats.jsx
@@ -10,22 +10,22 @@ function Stats(){
     // const ref = useRef(null);
 
     return( <div  className="bg-black text-center">
-        <h2 className="text-[6vw] mx-auto font-medium pt-4 font-Monstserrat text-white">Stats</h2>
+        <h2 className="text-[6vw] mx-auto font-medium pt-4 font-Monstserrat text-white ">Stats</h2>
 
         <div className="grid grid-flow-col grid-cols-3">
-           <div>
-                <h2 className="text-white font-Ruslan text-[8vw]">300</h2>
-                <h3 className="text-white font-Monstserrat text-[2vw]">Eggs Minted</h3>
+           <div className="bg-[#f0ffe9] pb-7 w-[80%] mx-auto rounded-xl border-l-8 border-[#97df75] drop-shadow-green">
+                <h2 className="text-[#97df75] font-Montserrat text-[7vw] font-extrabold">3000</h2>
+                <h3 className="text-[#97df75] font-bold font-Montserrat text-[2vw]">Total Supply</h3>
            </div>
                 
-           <div>
-                <h2 className="text-white font-Ruslan text-[8vw]">69</h2>
-                <h3 className="text-white font-Monstserrat text-[2vw]">Eggs Sold</h3>
+           <div className="bg-[#e1f8ff] pb-7 w-[80%] mx-auto rounded-xl border-b-8 border-[#7ae2ff]  drop-shadow-blue">
+                <h2 className="text-[#7ae2ff] font-Montserrat text-[7vw] font-extrabold">300</h2>
+                <h3 className="text-[#7ae2ff] font-bold font-Montserrat text-[2vw]">Minted</h3>
            </div>
 
-           <div>
-                <h2 className="text-white font-Ruslan text-[8vw]">2 ETH</h2>
-                <h3 className="text-white font-Monstserrat text-[2vw]">Volume Traded</h3>
+           <div className="bg-[#fff4e9] pb-7 w-[80%] mx-auto rounded-xl border-r-8 border-[#ffb56d] drop-shadow-orange">
+                <h2 className="text-[#ffb56d] font-Montserrat text-[7vw] font-extrabold">2 ETH</h2>
+                <h3 className="text-[#ffb56d] font-bold font-Montserrat text-[2vw]">Volume Traded</h3>
            </div>
        </div>
        </div>
--- beast-verse/tailwind.config.js
+++ beast-verse/tailwind.config.js
@@ -4,10 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        'blue': '0 10px 30px rgba(225, 248, 255, 0.5)',
+        'orange': '0 10px 30px rgba(255, 244, 233, 0.5)',
+        'green' : '0 10px 30px rgba(240, 255, 233, .5)',      
+      },
+    },
     fontFamily: {
       Ruslan:['Ruslan Display', 'sans-serif'],
-      Monstserrat: ['Montserrat', 'sans-serif'],
+      Montserrat: ['Montserrat', 'sans-serif'],
     },
   },
   plugins: [],
