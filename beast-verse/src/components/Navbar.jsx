e72f37f01a8b60f517edc34b6e6e8921c0b31337
SayakSarkar02 <sayaksarkar73@gmail.com>
2023-03-18 11:30:46 +0530

    dark nav

--- beast-verse/src/components/Navbar.jsx
+++ beast-verse/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ export default function Navbar() {
       delay: 7
     }}
 
-    className='bg-white w-[70%] mt-3 container mx-auto rounded-xl drop-shadow-xl'>
+    className='bg-gradient-to-br from-black to-slate-700 w-[70%] mt-3 container mx-auto rounded-xl drop-shadow-xl'>
         <div className='p-4 px-5 flex flex-row justify-between items-center max-h-20'>
             <div className='cursor-pointer'>
                 <img src={logo} alt="bv_logo" className='w-24' />
