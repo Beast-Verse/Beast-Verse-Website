2735a078dab234a262d034713aa92581157ca023
Risav03 <needleweed03@gmail.com>
2023-03-23 11:32:53 +0530

    hover kor

--- beast-verse/src/components/Aboutus.jsx
+++ beast-verse/src/components/Aboutus.jsx
@@ -1,20 +1,25 @@
 import React, { useRef} from "react";
-
 // import Ice from "../assets/Ice.png"
 import {
-  motion,
+  motion, Variants,
   useScroll,
-  // useInView,
+  useInView,
   useTransform,
 } from "framer-motion";
+
+
+
+
 import Left from "../assets/left.png";
 import Right from "../assets/Right.png";
 
 
-
-
 import All from "../assets/All.png";
 import { useState } from "react";
+
+
+
+
 
 
 
@@ -49,7 +54,7 @@ function AboutUs() {
     clamp: false,
   });
 
-  const moveY2 = useTransform(scrollYProgress, [0, .25, .5, .75, 1], ["0vw", "0vw", "0vw", "2vw", "4vw"], {
+  const moveY2 = useTransform(scrollYProgress, [0, .25, .5, .75, 1], ["0vw", "0vw", "0vw", "9vw", "15vw"], {
     clamp: false,
   });
 
@@ -78,14 +83,15 @@ function AboutUs() {
 
 
   return (
-    <div className=" w-screen pt-10 bg-black h-[120vw]" id="AboutUs">
+    <div className=" w-screen bg-black pt-10 h-[120vw]" id="AboutUs">
       {/* <motion.div style={{ scaleX: scrollYProgress }} /> */}
       {/* <motion.div className="origin-top-left fixed z-50 top-0 left-0 right-0 transform-none bg-red-500 h-2" style={{ scaleX: scrollYProgress}}/> */}
+      <div className="bg-gradient-to-b from-[#1b1008] to-black ">
       <h2 className="text-[4.5vw] text-center font-Montserrat font-semibold text-white">
         What is <span className=" text-green-500">Beast Verse</span> ?
       </h2>
 
-      <div className="grid grid-flow-col grid-cols-2 mt-20 mx-auto">
+      <div className="grid grid-flow-col grid-cols-2 mt-12 mx-auto">
         <div className="px-20">
           <p className="font-Montserrat text-[1.1vw] pt-4 text-white">
             
@@ -105,7 +111,7 @@ function AboutUs() {
 
         <div className="mx-auto">
           <iframe
-            className="rounded-2xl shadow-2xl shadow-blue-500/50"
+            className="rounded-2xl shadow-2xl shadow-green-500/50"
             width="560"
             height="315"
             src="https://www.youtube.com/embed/AMt5x44k1FY"
@@ -114,6 +120,8 @@ function AboutUs() {
             allowfullscreen
           ></iframe>
         </div>
+        
+      </div>
       </div>
 
         <h2 className="text-center text-white text-5xl mt-16 font-Montserrat font-semibold">See you in <span className=" text-green-500">Beast Verse!</span></h2>
@@ -165,27 +173,47 @@ function AboutUs() {
           <motion.img src={All} className="">
             
           </motion.img>
-          <div className="w-[15rem] absolute bottom-[11vw] left-[4.5vw] opacity-0 hover:opacity-100 transition-all duration-500 bg-green-400/30 text-center border-2 border-green-700 rounded-xl">
-            <h3 className="font-Montserrat font-bold text-green-500 pt-3 text-[1.7rem] mb-3">Common Eggs</h3>
-            <p className="text-white font-Montserrat px-4 pb-2">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.</p>
+          <div onClick={()=>setCommon(true) & setEpic(false) & setLegendary(false) & setRare(false)} className="w-[15rem] h-[25vw] min-[1500px]:h-[22vw] absolute bottom-[11vw] min-[1500px]:bottom-[13vw] left-[4.5vw] min-[1500px]:left-[6vw]">
+          <motion.div initial="0px" className=" w-[20rem] min-[1500px]:w-[25rem] absolute bottom-[0vw] left-[2vw] min-[1500px]:left-[2vw] border-2 border-green-500 bg-green-300/20 rounded-xl text-center">
+              <h1 className="text-green-200 text-[1.7vw] font-bold font-Montserrat pt-2">Common Eggs</h1>
+              <p className="font-Montserrat text-green-50 font-medium text-[1vw] min-[1500px]:text-[.89vw] px-2 pt-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+              </motion.div>   
           </div>
 
 
-          <div className="w-[15rem] absolute bottom-[11vw] left-[29vw] opacity-0 hover:opacity-100 transition-all duration-500 bg-blue-400/30 text-center border-2 border-blue-700 rounded-xl">
-            <h3 className="font-Montserrat font-bold text-blue-500 pt-3 text-[1.7rem] mb-3">Rare Eggs</h3>
-            <p className="text-white font-Montserrat px-4 pb-2">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.</p>
-          </div>
+          <div onClick={()=>setCommon(false) & setEpic(false) & setLegendary(false) & setRare(true)} className="w-[15rem] h-[25vw] min-[1500px]:h-[22vw] absolute bottom-[11vw] left-[29vw] min-[1500px]:bottom-[13vw] min-[1500px]:left-[31vw] ">
+            
+            </div>
 
-          <div className="w-[15rem] absolute bottom-[11vw] left-[55vw] opacity-0 hover:opacity-100 transition-all duration-500 bg-purple-400/30 text-center border-2 border-purple-700 rounded-xl">
-            <h3 className="font-Montserrat font-bold text-purple-500 pt-3 text-[1.7rem] mb-3">Epic Eggs</h3>
-            <p className="text-white font-Montserrat px-4 pb-2">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.</p>
-          </div>
+          <div onClick={()=>setCommon(false) & setEpic(true) & setLegendary(false) & setRare(false)} className="w-[15rem] h-[25vw] min-[1500px]:h-[22vw] absolute bottom-[11vw] left-[55vw] min-[1500px]:bottom-[13vw] min-[1500px]:left-[56.5vw]">
+             </div>
 
-          <div className="w-[15rem] absolute bottom-[11vw] left-[79.5vw] opacity-0 hover:opacity-100 transition-all duration-500 bg-yellow-400/30 text-center border-2 border-yellow-700 rounded-xl">
-            <h3 className="font-Montserrat font-bold text-yellow-500 pt-3 text-[1.7rem] mb-3">Legendary Eggs</h3>
-            <p className="text-white font-Montserrat px-4 pb-2">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.</p>
-          </div>
+          <div onClick={()=>setCommon(false) & setEpic(false) & setLegendary(true) & setRare(false)} className="w-[15rem] h-[25vw] min-[1500px]:h-[22vw] absolute bottom-[11vw] left-[78.5vw] min-[1500px]:bottom-[13vw] min-[1500px]:left-[81vw]">
+            </div>
+
+
+            {common && <motion.div initial="0px" className=" w-[20rem] min-[1500px]:w-[25rem] absolute bottom-[0vw] left-[2vw] min-[1500px]:left-[2vw] border-2 border-green-500 bg-green-300/20 rounded-xl text-center">
+              <h1 className="text-green-200 text-[1.7vw] font-bold font-Montserrat pt-2">Common Eggs</h1>
+              <p className="font-Montserrat text-green-50 font-medium text-[1vw] min-[1500px]:text-[.89vw] px-2 pt-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+              </motion.div>}
+
+              {rare && <div className="min-[1500px]:w-[25rem] w-[20rem] absolute bottom-[0vw] left-[26.5vw] min-[1500px]:left-[26.5vw] border-2 border-blue-500 bg-blue-300/20 rounded-xl text-center">
+              <h1 className="text-blue-200 text-[1.7vw]  font-bold font-Montserrat pt-2">Rare Eggs</h1>
+              <p className="font-Montserrat text-blue-50 font-medium text-[1vw] min-[1500px]:text-[.89vw] px-2 pt-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+              </div>}
+
+              {epic && <div className="min-[1500px]:w-[25rem] w-[20rem] absolute bottom-[0vw] left-[52.5vw] min-[1500px]:left-[53vw] border-2 border-purple-500 bg-purple-300/20 rounded-xl text-center">
+              <h1 className="text-purple-200 text-[1.7vw]  font-bold font-Montserrat pt-2">Epic Eggs</h1>
+              <p className="font-Montserrat text-purple-50 font-medium text-[1vw] min-[1500px]:text-[.89vw] px-2 pt-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+              </div>}
+
+              {legendary && <div className=" w-[20rem] absolute min-[1500px]:w-[25rem] bottom-[0vw] left-[76vw] min-[1500px]:left-[77vw] border-2 border-yellow-500 bg-yellow-300/20 rounded-xl text-center">
+              <h1 className="text-yellow-200 text-[1.7vw]  font-bold font-Montserrat pt-2">Legendary Eggs</h1>
+              <p className="font-Montserrat text-yellow-50 font-medium text-[1vw] min-[1500px]:text-[.89vw] px-2 pt-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+              </div>}
+
         </motion.div>
+
 
 
         </motion.div>
