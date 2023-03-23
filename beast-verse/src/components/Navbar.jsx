8b5e1eff26d421c0efc11f19f6c81b4df128ab29
Risav03 <needleweed03@gmail.com>
2023-03-23 23:39:00 +0530

    whitepaper

--- beast-verse/src/components/Navbar.jsx
+++ beast-verse/src/components/Navbar.jsx
@@ -21,12 +21,12 @@ export default function Navbar() {
                 <img src={logo} alt="bv_logo" className='w-24' />
             </div>
             <ul className='flex flex-row gap-5 font-Monstserrat font-bold text-blue-400 items-center'>
-
+            <li className='hover:text-blue-300 cursor-pointer'><Link to="https://drive.google.com/file/d/1voYANBhdLDcoXAcRt-CvAJRKbeqX6_YN/view?usp=sharing" smooth>Whitepaper</Link></li>
                 <li className='hover:text-blue-300 cursor-pointer'><Link to="#Landing" smooth>Home</Link></li>
                 <li className='text-gray-400 cursor-pointer'>MarketPlace</li>
                 <li className='text-gray-400 cursor-pointer'>Buy Eggs</li>
                 <li className='hover:text-blue-300 cursor-pointer'><Link to="#AboutUs" smooth>About Us</Link></li>
-                <li className='w-[2px] h-8 bg-gray-300'></li>
+
                 <li className='bg-gradient-to-br from-[#6c7aca] to-[#8C9EFF] hover:to-[#8292EA] hover:from-[#6c7aca] text-white py-2 px-5 rounded-full cursor-pointer'><a href="https://discord.gg/JG9VAuJbD7">Join Discord</a></li>
                 <li className='bg-gradient-to-br from-green-600 to-lime-400 hover:to-green-600 hover:from-lime-500 text-white py-2 px-5 rounded-full cursor-pointer'><a href="https://forms.gle/kNn1TArMvbYRc3vj8">Join Waitlist</a></li>
 
