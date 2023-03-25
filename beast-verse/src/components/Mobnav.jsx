3863d150a9e6b08217a2d9effecc3031188bdcfc
Risav03 <needleweed03@gmail.com>
2023-03-25 12:22:09 +0530

    final nav

--- beast-verse/src/components/Mobnav.jsx
+++ beast-verse/src/components/Mobnav.jsx
@@ -21,24 +21,30 @@ function Mobnav() {
        
         <ul className={`${open?'block':'hidden'} w-fit text-center mx-auto `}>
           <li >
-            <div className="grid grid-flow-col grid-cols-4 mx-auto gap-2 ">
-            <Link className="text-blue-300 text-[2.8vw]  font-Montserrat font-bold  rounded-xl px-3 py-4 bg-gradient-to-br from-slate-900 to-slate-700 mb-2" to="#Mobland" smooth>
-            Home
-          </Link>
-          <h3 className="text-gray-300 text-[2.8vw]  font-Montserrat font-bold rounded-xl px-3 py-4 bg-gradient-to-br from-slate-900 to-slate-700 mb-2">MarketPlace</h3>
-          <h3 className="text-gray-300 text-[2.8vw]  font-Montserrat font-bold rounded-xl px-3 py-4 bg-gradient-to-br from-slate-900 to-slate-700 mb-2">Buy Eggs</h3>
-          <Link className="text-blue-300 text-[2.8vw]  font-Montserrat font-bold rounded-xl px-3 py-4 bg-gradient-to-br from-slate-900 to-slate-700 mb-2" to="#Mobabout" smooth>
+            <div className="grid grid-flow-col grid-cols-2 mx-auto gap-2 ">
+            <h3 className="text-gray-300 text-[4vw]  font-Montserrat font-bold rounded-xl px-3 py-4 bg-gradient-to-br from-slate-900 to-slate-700 mb-2">Buy Eggs</h3>
+          <Link className="text-blue-300 text-[4vw]  font-Montserrat font-bold rounded-xl px-3 py-4 bg-gradient-to-br from-slate-900 to-slate-700 mb-2" to="#Mobabout" smooth>
           About Us
           </Link>
+          </div>
+          </li>
+
+          <li >
+            <div className="grid grid-flow-col grid-cols-2 mx-auto gap-2 ">
+            <Link className="text-blue-300 text-[4vw]  font-Montserrat font-bold  rounded-xl px-3 py-4 bg-gradient-to-br from-slate-900 to-slate-700 mb-2" to="#Mobland" smooth>
+            Home
+          </Link>
+          <h3 className="text-gray-300 text-[4vw]  font-Montserrat font-bold rounded-xl px-3 py-4 bg-gradient-to-br from-slate-900 to-slate-700 mb-2">MarketPlace</h3>
+          
           </div>
           </li>
            
           
           <li >
 
-           <div className="grid grid-flow-row grid-cols-2 gap-4 w-[80%] mx-auto mt-2">
-           <a className=" mb-2 rounded-xl py-4 bg-gradient-to-br from-[#6c7aca] to-[#8C9EFF]  text-white text-[3vw]  font-Montserrat font-bold " href="https://discord.gg/JG9VAuJbD7">Join Discord</a>
-            <a className=" mb-2 rounded-xl py-4 bg-gradient-to-br from-green-600 to-lime-400  text-white text-[3vw]  font-Montserrat font-bold " href="https://forms.gle/kNn1TArMvbYRc3vj8">Join Waitlist</a>
+           <div className="grid grid-flow-row grid-cols-2 gap-4 mt-2">
+           <a className=" mb-2 rounded-xl py-5 bg-gradient-to-br from-[#6c7aca] to-[#8C9EFF]  text-white text-[4vw]  font-Montserrat font-bold " href="https://discord.gg/JG9VAuJbD7">Join Discord</a>
+            <a className=" mb-2 rounded-xl py-5 bg-gradient-to-br from-green-600 to-lime-400  text-white text-[4vw]  font-Montserrat font-bold " href="https://forms.gle/kNn1TArMvbYRc3vj8">Join Waitlist</a>
           </div>
             </li>
         </ul>
