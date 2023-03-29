d5f14e90208b2353c37a1d551e688929f51ad75a
SayakSarkar02 <sayaksarkar73@gmail.com>
2023-03-29 22:23:17 +0530

    black

--- beast-verse/src/components/Partners.jsx
+++ beast-verse/src/components/Partners.jsx
@@ -3,7 +3,7 @@ import nftcal from '../assets/partners/nftcal.svg';
 
 export default function Partners() {
   return (
-    <div className='bg-gradient-to-b from-black to-slate-800 text-center py-20 px-40 pb-32 max-[740px]:px-10'>
+    <div className='bg-black text-center py-20 px-40 pb-32 max-[740px]:px-10'>
         <h2 className='text-6xl font-bold font-Montserrat text-blue-500 pb-10 text-center max-[740px]:text-4xl'>PARTNERS</h2>
         <div className='flex flex-row items-start justify-center'>
             <div className=' flex flex-col items-center'>
