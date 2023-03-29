cd3dab6b05c73bdaf0ebbb2d45365141f758fbe3
Risav03 <needleweed03@gmail.com>
2023-03-29 22:33:46 +0530

    linked

--- beast-verse/src/components/Partners.jsx
+++ beast-verse/src/components/Partners.jsx
@@ -10,10 +10,10 @@ export default function Partners() {
                 <div className='h-5 overflow-hidden'>
                 <div className='bg-blue-500 relative top-0 left-0 w-10 h-10 rounded-full'></div>
                 </div>
-                <div className='p-10 pt-4 border border-t-4 border-blue-500 border-x-0 border-b-0 rounded-xl bg-gradient-to-br from-blue-400/20 to-slate-900/70 flex flex-col items-center justify-center'>
+                <a href="https://nftcalendar.io/"><div className='p-10 pt-4 border border-t-4 border-blue-500 border-x-0 border-b-0 rounded-xl bg-gradient-to-br from-blue-400/20 to-slate-900/70 flex flex-col items-center justify-center'>
                     <img src={nftcal} alt="nftcal" className='w-40'/>
                     <h3 className='text-white font-Montserrat text-2xl'>NFT Calendar</h3>
-                </div>
+                </div></a>
             </div>
         </div>
     </div>
