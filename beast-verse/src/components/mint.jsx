b6227408863951a1422fe6628826c09104dd99dc
anuHait <anusmitahait67@gmail.com>
2023-04-12 01:20:47 +0530

    eggs added

--- beast-verse/src/components/mint.jsx
+++ beast-verse/src/components/mint.jsx
@@ -1,30 +1,36 @@
 import React from 'react'
-import beast from "../assets/beast.jpeg"
+//import beast from "../assets/beast.jpeg"
 import Footer from './Footer'
+import common from '../assets/Common.gif'
+import epic from '../assets/Epic.gif'
+import Legendary from '../assets/Legendary.gif'
+import Rare from '../assets/Rare.gif'
 function Mint() {
   const eggs=[
     {
       name:"hi1",
-      image:beast,
-      css:"bg-[#00BB02] text-[#93FF94] border-[#93FF94]"
+      image:common,
+      css:"bg-[#00BB02] text-[#93FF94] border-[#93FF94]",
+      imgcss:"shadow-xl shadow-[#93FF94]",
     },
     {
       name:"hi2",
-      image:beast,
-      css:"bg-[#007DBB] text-[#93E5FF] border-[#93E5FF]"
-
+      image:Rare,
+      css:"bg-[#007DBB] text-[#93E5FF] border-[#93E5FF]",
+      imgcss:"shadow-xl shadow-[#93E5FF]",
     },
     {
       name:"hi3",
-      image:beast,
-      css:"bg-[#9124CB] text-[#E093FF] border-[#E093FF]"
+      image:epic,
+      css:"bg-[#9124CB] text-[#E093FF] border-[#E093FF]",
+      imgcss:"shadow-xl shadow-[#E093FF]",
 
     },
     {
       name:"hi4",
-      image:beast,
-      css:"bg-[#D8D600] text-[#FEFFB6] border-[#FEFFB6]"
-
+      image:Legendary,
+      css:"bg-[#D8D600] text-[#FEFFB6] border-[#FEFFB6]",
+      imgcss:"shadow-xl shadow-[#FEFFB6]",
     },
 
   ]
@@ -37,8 +43,8 @@ function Mint() {
       {
         eggs.map((egg)=>(
           <div className='flex flex-col m-10 justify-center items-center '>
-          <img src={egg.image} className=' w-[28.5vh] rounded-lg md:w-[35.2vh] pb-8 dr'></img>
-          <button className={`font-bold text-3xl text-slate-50 border-2 ${egg.css} rounded-xl p-3 w-[33vw] md:w-[11vw]`}>MINT</button>
+          <img src={egg.image} className={`w-[28.5vh]  md:w-[35.2vh] rounded-lg ${egg.imgcss}`}></img>
+          <button className={`font-bold text-3xl text-slate-50 border-2 ${egg.css} rounded-xl p-3 w-[33vw] md:w-[11vw] mt-10`}>MINT</button>
           </div>
         ))
       }
