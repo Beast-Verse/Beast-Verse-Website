6adff1e0f1238b9be84e5ff63ea65fd14720744d
anuHait <anusmitahait67@gmail.com>
2023-04-16 11:50:00 +0530

    minor changes

--- beast-verse/src/components/mint.jsx
+++ beast-verse/src/components/mint.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 //import beast from "../assets/beast.jpeg"
 import Footer from './Footer'
-import common from '../assets/Common.gif'
-import epic from '../assets/Epic.gif'
-import Legendary from '../assets/Legendary.gif'
-import Rare from '../assets/Rare.gif'
+import common from '../assets/Commons.gif'
+import epic from '../assets/Epics.gif'
+import Legendary from '../assets/Legendarys.gif'
+import Rare from '../assets/Rares.gif'
 
 import Web3 from "web3"
 const web3 = new Web3(window.ethereum);
@@ -662,7 +662,7 @@ var ADDRESS = "0x081Af03e7A316FC54f39b1A1ea9d75c70DBDb442";
       console.log(typeof(account));
      
 const whitelisted = ["0xc9de0a09b6e547cf7e028aabb7b1f2f6941ad53f", "0xd3057d40d6538f32489CA3ce73e2A2E60405F0A1", "0x4012dBd303447EC2EF495f18c1f95CCf01a33594", "0xb1574f6638f835a292647D5E7e09Af91E947Bb2A", "0xDA626847B42e914FAa71E46055e57b20A066bed8", "0x071e2dbcf04cD810Cd1b89E3d8276D598f3B31E8", "0x9d7AF80C1c9007E280288714B3dd68Aa587342B3", "0x26c22e7cE4B374f7219398d0B90F354a780c2328", "0x5FDd13F8Bd76C4500DbC8eE9bdd1dD3D3ee041f9", "0x77e058DE3284F4e8bC9Aaa5bDa657E61e9c54114", "0x9eeab55272f38f624a56995cd719d577364e2a7c", "0x3a84ba8CAFb1730e72a3DA52456CB8D95034de9c", "0xebd269E7ED51EDE698CE92F41A8F45034c8CFe5B", "0x783433375a9829B647E28fe82F81A20989cDc8b9", "0x65b835ec6802b2C35b93092bBd76216BB3018F50", "38xCgnfcLEb2kP45L1ZT5L3BFzskhQqbE5", "0xE633567d20749bF0025a7C748c93542A413Eeb3A", "0xE2a45eA9F3F614aD325F4E02669B505157846669", "0x3E63a6fcabB3e8bbc9c9e11c95D014Ec46B49baa", "0x00a22e41cb5Ea46e643f8eC06526A9670c77fF1e", "0x09878440535ed004cc20732c19821befb7c04cDb", 
-"0x2f40e27e1f3d960716eebe8587166e4bdbb33bcb97055e23c156c24132a34789", "0x3B904247bAC041E53a7e6842F7D08F3b6700779f", "0xDda4fE2Bf07bA47D4cc4ffb10f01c964f1aCE9e8", "0xD99100638eaA63C621E640170943dEFe9de1af10","0x1ce256752fBa067675F09291d12A1f069f34f5e8", "0xc67Aa95B4AD61b6435d10567EC192e125aF7A0a0"];
+"0x2f40e27e1f3d960716eebe8587166e4bdbb33bcb97055e23c156c24132a34789", "0x3B904247bAC041E53a7e6842F7D08F3b6700779f", "0xDda4fE2Bf07bA47D4cc4ffb10f01c964f1aCE9e8", "0xD99100638eaA63C621E640170943dEFe9de1af10","0x1ce256752fBa067675F09291d12A1f069f34f5e8", "0xc67Aa95B4AD61b6435d10567EC192e125aF7A0a0","0xd762eb9a13821a93e5c02baefEC29fC1E785FaF2"];
 
 const size = whitelisted.length;
 
@@ -796,33 +796,33 @@ function Mint() {
   return (
     <>
 
-    <div className='bg-gradient-to-b from-black to-slate-800 text-center h-full md:h-[100vh]'>
+    <div className='bg-gradient-to-b from-black to-slate-800 text-center h-full '>
       <h1 className='pt-10 text-5xl font-bold text-blue-400  lg:text-7xl' id="check">MINT YOUR EGGS</h1>
       {/* <h2 id="success" className='mt-10 text-[3vw] font-Montserrat font-semibold text-green-400 lg:text-[5vw]'></h2> */}
-      <div className='grid  grid-rows-4  md:grid-flow-cols md:grid-cols-4 mt-20 w-[90%] mx-auto gap-10 text-3xl text-white font-Montserrat' id="wlonly">
+      <div className='grid  grid-rows-4  md:grid-flow-cols md:grid-cols-4 mt-20 w-[90%] h-fit mx-auto gap-10 text-3xl text-white font-Montserrat' id="wlonly">
       
           <div className='grid grid-flow-row grid-rows-2 mx-auto justify-center items-center '>
-          <img src={common} className="w-[29.5vh]  md:w-[40.2vh] rounded-lg shadow-xl shadow-[#93FF94]"></img>
+          <img src={common} className="w-[29.5vh]  md:w-[40.2vh] rounded-lg shadow-xl shadow-[#93FF94] brightness-150"></img>
           <button id="mint common" className="font-bold text-3xl border-2 mx-auto rounded-xl p-3 w-[30vw] md:w-[11vw]  bg-[#00BB02] text-[#c5ffc6] border-[#93FF94]">MINT</button>
           </div>
 
           <div className='grid grid-flow-row grid-rows-2 mx-auto justify-center items-center '>
-          <img src={Rare} className="w-[29.5vh]  md:w-[40.2vh] rounded-lg shadow-xl shadow-[#93E5FF]"></img>
+          <img src={Rare} className="w-[29.5vh]  md:w-[40.2vh] rounded-lg shadow-xl shadow-[#93E5FF] brightness-125"></img>
           <button id="mint rare" className="font-bold text-3xl border-2 mx-auto rounded-xl p-3 w-[30vw] md:w-[11vw]  bg-[#007DBB] text-[#b4edff] border-[#93E5FF]">MINT</button>
           </div>
 
           <div className='grid grid-flow-row grid-rows-2 mx-auto justify-center items-center '>
-          <img src={epic} className="w-[29.5vh]  md:w-[40.2vh] rounded-lg shadow-xl shadow-[#E093FF]"></img>
+          <img src={epic} className="w-[29.5vh]  md:w-[40.2vh] rounded-lg shadow-xl shadow-[#E093FF] brightness-125"></img>
           <button id="mint epic" className="font-bold text-3xl border-2 mx-auto rounded-xl p-3 w-[30vw] md:w-[11vw]  bg-[#9124CB] text-[#e8b2fd] border-[#E093FF]">MINT</button>
           </div>
 
           <div className='grid grid-flow-row grid-rows-2 mx-auto justify-center items-center '>
-          <img src={Legendary} className="w-[29.5vh]  md:w-[40.2vh] rounded-lg shadow-xl shadow-[#FEFFB6]"></img>
+          <img src={Legendary} className="w-[29.5vh]  md:w-[40.2vh] rounded-lg shadow-xl shadow-[#FEFFB6] brightness-125"></img>
           <button id="mint legendary" className="font-bold text-3xl border-2 mx-auto rounded-xl p-3 w-[30vw] md:w-[11vw]  bg-[#D8D600] text-[#feffcc] border-[#FEFFB6]">MINT</button>
           </div>
 		  
       </div>
-	  <button id='withdraw' className='bg-white rounded-xl font-Montserrat text-2xl p-3 font-semibold border-2 border-blue-400'>Withdraw</button>
+	  <button id='withdraw' className='bg-white rounded-xl font-Montserrat text-2xl p-3 font-semibold border-2 border-blue-400 '>Withdraw</button>
     </div>
    
     </>
