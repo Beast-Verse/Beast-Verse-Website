0061e7e825b0fdac9a00d3af2616ac7905bb5267
Risav03 <needleweed03@gmail.com>
2023-04-16 20:53:04 +0530

    go shit

--- beast-verse/src/components/mint.jsx
+++ beast-verse/src/components/mint.jsx
@@ -653,8 +653,7 @@ var ADDRESS = "0x081Af03e7A316FC54f39b1A1ea9d75c70DBDb442";
 const admin = "0xc67Aa95B4AD61b6435d10567EC192e125aF7A0a0";
 
 
-
-(async ()=> {
+async function connect() {
   
   if(window.ethereum){
     console.log("Metamask is here.");
@@ -664,6 +663,17 @@ const admin = "0xc67Aa95B4AD61b6435d10567EC192e125aF7A0a0";
       const accounts = await window.ethereum.request({method: "eth_requestAccounts",});
       account = accounts[0];
       console.log(typeof(account));
+
+
+
+if(account != null){
+	document.getElementById("buttonconnect").textContent = "Wallet already Connected"
+	document.getElementById("address").textContent = account;
+	document.getElementById("buttonconnect").classList.remove("bg-white");
+	document.getElementById("buttonconnect").classList.add("text-gray-500");
+	document.getElementById("buttonconnect").classList.add("bg-gray-300");
+}
+
      
 const whitelisted = ["0xc9de0a09b6e547cf7e028aabb7b1f2f6941ad53f", "0xd3057d40d6538f32489CA3ce73e2A2E60405F0A1", "0x4012dBd303447EC2EF495f18c1f95CCf01a33594", "0xb1574f6638f835a292647D5E7e09Af91E947Bb2A", "0xDA626847B42e914FAa71E46055e57b20A066bed8", "0x071e2dbcf04cD810Cd1b89E3d8276D598f3B31E8", "0x9d7AF80C1c9007E280288714B3dd68Aa587342B3", "0x26c22e7cE4B374f7219398d0B90F354a780c2328", "0x5FDd13F8Bd76C4500DbC8eE9bdd1dD3D3ee041f9", "0x77e058DE3284F4e8bC9Aaa5bDa657E61e9c54114", "0x9eeab55272f38f624a56995cd719d577364e2a7c", "0x3a84ba8CAFb1730e72a3DA52456CB8D95034de9c", "0xebd269E7ED51EDE698CE92F41A8F45034c8CFe5B", "0x783433375a9829B647E28fe82F81A20989cDc8b9", "0x65b835ec6802b2C35b93092bBd76216BB3018F50", "38xCgnfcLEb2kP45L1ZT5L3BFzskhQqbE5", "0xE633567d20749bF0025a7C748c93542A413Eeb3A", "0xE2a45eA9F3F614aD325F4E02669B505157846669", "0x3E63a6fcabB3e8bbc9c9e11c95D014Ec46B49baa", "0x00a22e41cb5Ea46e643f8eC06526A9670c77fF1e", "0x09878440535ed004cc20732c19821befb7c04cDb", 
 "0x2f40e27e1f3d960716eebe8587166e4bdbb33bcb97055e23c156c24132a34789", "0x3B904247bAC041E53a7e6842F7D08F3b6700779f", "0xDda4fE2Bf07bA47D4cc4ffb10f01c964f1aCE9e8", "0xD99100638eaA63C621E640170943dEFe9de1af10","0x1ce256752fBa067675F09291d12A1f069f34f5e8", "0xc67Aa95B4AD61b6435d10567EC192e125aF7A0a0","0xd762eb9a13821a93e5c02baefEC29fC1E785FaF2", "0xCfbA620CB5f1b6FeaAd4050e8494BDcB0edA5fbb"];
@@ -786,14 +796,17 @@ console.log(isthere);
 		document.getElementById("wlonly").textContent="Your wallet was not whitelisted! \n Please wait for the General Mint to start on 21/05/23! :D"
 		document.getElementById("noview").innerHTML=<div></div>
 		}
-	}
+
 
 
 
   else{
     console.log("no metamask");
   }
-})();
+};
+
+}
+
 
 function Mint() {
   
@@ -801,7 +814,9 @@ function Mint() {
     <>
 
     <div className='bg-gradient-to-b from-black to-slate-800 text-center min-h-[100vh] flex flex-col items-center justify-center max-[768px]:pt-10 max-[768px]:justify-start p-2'>
-      <h1 className='pt-10 text-4xl font-bold text-blue-400  lg:text-6xl' id="check">MINT YOUR EGGS</h1>
+      <h1 className='pt-10 text-4xl font-bold text-blue-400  lg:text-6xl mb-10' id="check">MINT YOUR EGGS</h1>
+	  <button className='bg-white p-4 rounded-xl font-Montserrat font-semibold' onClick={connect} id="buttonconnect">Connect your wallet</button>
+	  <h3 id="address" className='mt-10 text-[1vw] mx-auto w-[70%] font-Montserrat font-medium text-green-400 lg:text-[1.5vw]'></h3>
       <h2 id="wlonly" className='mt-10 text-[3vw] mx-auto w-[70%] font-Montserrat font-medium text-slate-400 lg:text-[3vw]'></h2>
       <div className='flex flex-row max-[768px]:flex-row max-[768px]:flex-wrap min-[768px]:mx-10' id="noview">
       
@@ -826,7 +841,7 @@ function Mint() {
           </div>
 		  
       </div>
-	  <button id='withdraw' className='bg-white rounded-xl font-Montserrat text-2xl p-3 font-semibold border-2 border-blue-400'>Withdraw</button>
+	  {/* <button id='withdraw' className='bg-white rounded-xl font-Montserrat text-2xl p-3 font-semibold border-2 border-blue-400'>Withdraw</button> */}
     </div>
    
     </>
